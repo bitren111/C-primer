0d9e403dcdecc79ecf0f9507732f90119faf99e4
bitren111 <13269182958@163.com>
2018-10-14 22:33:35 +0800

    gq

--- Data_Structure_And_Algorithm/Data_Structure_And_Algorithm/SortTestHelper.h
+++ Data_Structure_And_Algorithm/Data_Structure_And_Algorithm/SortTestHelper.h
@@ -177,4 +177,30 @@ void mergeSortBU(T arr[], int n) {
 	}
 }
 
+template<typename T>
+void quickSort(T arr[], int n) {
+	__quickSort(arr, 0, n - 1);
+}
+template<typename T>
+void __quickSort(T arr[], int l, int r) {
+	if (l <= r)
+		return;
+	int p = __partition(arr, l, r);
+	__quickSort(arr, l, p - 1);
+	__quickSort(arr, p + 1, r);
+}
+
+template<typename T>
+int __partition(T arr[], int l, int r) {
+	T v = arr[l];
+	int j = l;
+	for (int i = l + 1; i <= r; i++) {
+		if (arr[i] < v) {
+			swap(arr[j + 1], arr[i]);
+			j++;
+		}
+	}
+	swap(arr[l], arr[j]);
+	return j;
+}
 #endif // !SortTestHelper_H
