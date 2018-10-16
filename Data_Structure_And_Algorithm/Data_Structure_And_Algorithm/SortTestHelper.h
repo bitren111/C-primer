bb2ea5127c8597680484182d0331240295e90cbf
bitren111 <13269182958@163.com>
2018-10-16 21:23:01 +0800

    gq

--- Data_Structure_And_Algorithm/Data_Structure_And_Algorithm/SortTestHelper.h
+++ Data_Structure_And_Algorithm/Data_Structure_And_Algorithm/SortTestHelper.h
@@ -178,29 +178,103 @@ void mergeSortBU(T arr[], int n) {
 }
 
 template<typename T>
+int __partition(T arr[], int l, int r) {
+	swap(arr[l], arr[rand() % (r - l + 1) + l]);
+	T v = arr[l];
+	int j = l;
+	for (int i = l + 1; i <= r; i++) {
+		if (arr[i] < v) {
+			j++;
+			swap(arr[j], arr[i]);
+		}
+	}
+	swap(arr[l], arr[j]);
+	return j;
+}
+
+template<typename T>
 void quickSort(T arr[], int n) {
+	srand(time(NULL));
 	__quickSort(arr, 0, n - 1);
 }
+
 template<typename T>
 void __quickSort(T arr[], int l, int r) {
-	if (l <= r)
+	if (l >= r)
 		return;
 	int p = __partition(arr, l, r);
 	__quickSort(arr, l, p - 1);
 	__quickSort(arr, p + 1, r);
 }
 
+
 template<typename T>
-int __partition(T arr[], int l, int r) {
+void __quickSort2(T arr[], int l, int r) {
+	if (r - l <= 15) {
+		Insert_Sort(arr, l, r);
+		return;
+	}
+	int p = __partition2(arr, l, r);
+	__quickSort2(arr, l, p - 1);
+	__quickSort2(arr, p + 1, r);
+}
+
+template<typename T>
+void quickSort2(T arr[], int n) {
+	__quickSort2(arr, 0, n - 1);
+}
+
+template<typename T>
+int __partition2(T arr[], int l, int r) {
+	swap(arr[l], arr[rand() % (r - l + 1) + l]);
 	T v = arr[l];
-	int j = l;
-	for (int i = l + 1; i <= r; i++) {
-		if (arr[i] < v) {
-			swap(arr[j + 1], arr[i]);
-			j++;
-		}
+	//arr[l+1,i)<v arr(j,r]>v
+	int i = l+1, j = r;
+	while (true) {
+		while (i <= r&&arr[i] < v) i++;
+		while (j >=l+1&&arr[j] > v) j--;
+		if (i > j)
+			break;
+		swap(arr[i], arr[j]);
+		i++; 
+		j--;
 	}
 	swap(arr[l], arr[j]);
 	return j;
+}
+
+template<typename T>
+void quickSort3(T arr[], int n) {
+	__quickSort3(arr, 0, n - 1);
+}
+
+template<typename T>
+void __quickSort3(T arr[], int l, int r) {
+	if (r - l <= 15) {
+		Insert_Sort(arr, l, r);
+		return;
+	}
+	swap(arr[l], arr[rand() % (r - l + 1) + l]);
+	T v = arr[l];
+	//arr[l+1,lt]<v arr[gt,r]>v
+	int lt = l, gt = r+1;
+	int i = l + 1; //arr[lt+1,i)==v
+	while (i<gt) {
+		if (arr[i] < v) {
+			swap(arr[i], arr[lt + 1]);
+			lt++;
+			i++
+		}
+		else if (arr[i] > v) {
+			swap(arr[i], arr[gt - 1]);
+			gt--;
+		}
+		else {
+			i++;
+		}
+	}
+	swap(arr[l], arr[lt]);
+	__quickSort3(arr, l, lt);
+	__quickSort3(arr, gt, r);
 }
 #endif // !SortTestHelper_H
--- Data_Structure_And_Algorithm/Data_Structure_And_Algorithm/main.cpp
+++ Data_Structure_And_Algorithm/Data_Structure_And_Algorithm/main.cpp
@@ -2,12 +2,31 @@
 #include"Student.h"
 
 int main() {
-	int n = 50000;
-	int *arr = SortHelper::generateRandomArray(n, 0, n);
-	int* arr2 = SortHelper::copyArray(arr, n);
-	SortHelper::testSort("Merge Sort BU", mergeSortBU, arr, n);
-	SortHelper::testSort("Merge Sort", merge_Sort, arr2, n);
-	delete[] arr;
+	int n = 100000;
+	int* arr1 = SortHelper::generateRandomArray(n, 0, n);
+	int* arr2 = SortHelper::copyArray(arr1, n);
+	SortHelper::testSort("Merge Sort", merge_Sort, arr1, n);
+	SortHelper::testSort("Quick Sort", quickSort, arr2, n);
+	delete[] arr1;
 	delete[] arr2;
+
+	int swapTimes = 100;
+	std::cout << "Test for  Random Nearly Ordered Array, size= " << n << ", swap time =" << swapTimes << endl;
+	arr1 = SortHelper::generateNearlyOrderedArray(n, swapTimes);
+	arr2 = SortHelper::copyArray(arr1, n);
+	SortHelper::testSort("Merge Sort", merge_Sort, arr1, n);
+	SortHelper::testSort("Quick Sort", quickSort, arr2, n);
+	delete[] arr1;
+	delete[] arr2;
+	std::cout << std::endl;
+
+	cout << "Test for Random Array, size = " << n << ", random range [0,10]" << endl;
+	arr1 = SortHelper::generateRandomArray(n, 0, 10);
+	arr2 = SortHelper::copyArray(arr1, n);
+	SortHelper::testSort("Merge Sort", merge_Sort,arr1, n);
+	SortHelper::testSort("Quick Sort", quickSort2, arr2, n);
+	delete[] arr1;
+	delete[] arr2;
+	std::cout << std::endl;
 	return 0;
 }
